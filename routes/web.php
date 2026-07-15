0c6aa51c7580ea1788e48ea79a0707ca03c177a0
ali jrewa <alifares.cr7aa@gmail.com>
2026-07-15 13:25:58 +0300

    fix : add get('/') to routes for login

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::middleware('guest')->group(function () {
+
+    Route::get('/', [AuthController::class, 'login']);
     Route::get('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/login', [AuthController::class, 'login_post']);
 });
